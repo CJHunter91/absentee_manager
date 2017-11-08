c8c0cad3797ff46f7e8b94ac55ce1d1579cad678
Chris Hunter <orkem2@gmail.com>
2017-11-08 16:31:02 +0000

    moves inline css from modal to css file

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -8,44 +8,10 @@ render(){
   if (this.props.isOpen === false)
      return null
 
- let modalStyle = {
-   position: 'fixed',
-   top: '50%',
-   left: '50%',
-   height: '40%',
-   width: '60%',
-   transform: 'translate(-50%, -50%)',
-   zIndex: '9999',
-   background: '#1E252D',
-   borderRadius: '10px',
-   border: 'solid 2px'
- }
-
- if (this.props.style) {
-   for (let key in this.props.style) {
-     modalStyle[key] = this.props.style[key]
-   }
- }
-
- let backdropStyle = {
-   position: 'fixed',
-   width: '100%',
-   height: '100%',
-   top: '0px',
-   left: '0px',
-   zIndex: '9998',
-   background: 'rgba(0, 0, 0, 0.3)'
- }
-
- if (this.props.backdropStyle) {
-   for (let key in this.props.backdropStyle) {
-     backdropStyle[key] = this.props.backdropStyle[key]
-   }
- }
  return(
 
   <article >
-    <section id="user-input" className="modal-content" style={modalStyle}>
+    <section id="user-input" className="modal-content">
     <span id="close" onClick={this.props.closeModal} className="close">&times;</span>
       <h4>Absence Request</h4>
       <form onSubmit={this.props.submitData}>
@@ -73,7 +39,7 @@ render(){
         {this.props.clash}
       </div>
     </section>
-    <div style={backdropStyle} onClick={e => this.close(e)}/>
+    <div id="backdrop" onClick={e => this.close(e)}/>
   </article>
   )
 }
